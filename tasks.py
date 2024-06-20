4eae08d8137532f54b26abd688cfb6caa1fad2b6
frank-git-15 <franciscoalvarrera15@gmail.com>
2024-06-19 19:11:50 -0600

    function webScrapper added, this funcion open an incognito chrome window and also give the possibliti of use chrome in background

--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 from robocorp.tasks import task
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 import time
 
@@ -14,15 +15,39 @@ class Article:
         self.imgeUrl = imgeUrl
 
 
+def webScrapper(is_background=True):
+    # Configute Chrome options
+    options = Options()
+
+    # Configuraciones adicionales
+    options.add_argument("--disable-extensions")#Disable extensions of chrome
+    options.add_argument("--profile-directory=Default")#use the default profile
+    options.add_argument("--incognito")#Ensure that execution is not affected by cookies, browser cache or hystory
+    options.add_argument("--start-maximized")#Maximize chrome window
+    if is_background:
+        # Execute the bot in background
+        options.add_argument("--headless")  # Execute the script without graphic interface
+        options.add_argument("--disable-gpu")  # Disable the use of GPU
+        options.add_argument("--window-size=1920,1080")  # Set a window size
+    with webdriver.Chrome() as driver:
+        driver.implicitly_wait(10)
+        driver.get(f"https://www.aljazeera.com/search/{search_criteria}?sort=date")
+
+        
+
+
+    
+
+
+
+
+
 
 @task
 def minimal_task():
-    with webdriver.Chrome() as driver:
-        articles_list = []
-        driver.implicitly_wait(10)
-        # driver.set_window_size(1024, 768)
+    webScrapper(False)
 
-        driver.get(f"https://www.aljazeera.com/search/{search_criteria}?sort=date")
 
-        time.sleep(5)
+
+        
 
