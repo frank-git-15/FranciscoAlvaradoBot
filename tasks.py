f8dea88dc4904e2162644df833034431f8056c31
frank-git-15 <franciscoalvarrera15@gmail.com>
2024-06-20 11:05:05 -0600

    Each time that an article is created, in the contructor the post date is extracted from description

--- tasks.py
+++ tasks.py
@@ -5,15 +5,105 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 import time
+import re
+from datetime import datetime,timedelta
+from dateutil.relativedelta import relativedelta
 
 
 search_criteria = "Biden"
+num_of_moths_to_retrieve = 1
 
 class Article:
     def __init__(self,header,description,imgeUrl) -> None:
         self.header = header
         self.description = description
         self.imgeUrl = imgeUrl
+        self.date_publish = self.__extractDatePublication(self.description)
+
+    def __extractDate(self,text):
+        pattern_months = r"(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)\s(\d{1,2}),\s(\d{4})"
+        matches = re.findall(pattern_months, text)
+
+        if matches:
+            date = "/".join(list(matches[0]))
+            return date
+        else: 
+            return None
+    def __extractTimeAgo(self,text):
+    
+        pattern = r'(.*)\s+ago\s+\.{1,3}'
+        matches = re.findall(pattern, text)
+
+        if matches:
+            return matches[0]
+        else:
+            return None
+    def __getPublicationDateFromTimeAgo(self,timeAgo):
+    
+        parts = timeAgo.split()
+        try:
+            amount = int(parts[0])
+        except Exception as e:
+            print(e)
+            return None
+        unit = str(parts[1]).lower()
+        
+        currentDate = datetime.now()
+        
+        if unit == "minutes" or unit == "minute":
+            newDate = currentDate - timedelta(minutes = amount)
+        elif unit == "hours" or unit == "hour":
+            newDate = currentDate - timedelta(hours = amount)
+        elif unit == "days" or unit == "day":
+            newDate = currentDate - timedelta(days = amount)
+        else:
+            newDate = None
+        try:
+            newDate = newDate.strftime("%b/%d/%Y")
+        except:
+            newDate = None
+        return newDate
+    def __extractDatePublication(self,description):
+
+        datePublication = self.__extractDate(description)
+        timeAgo = self.__extractTimeAgo(description)
+        datePublicationOfArticle = None
+
+        if datePublication is not None:
+
+
+            datePublicationOfArticle = datePublication
+        elif timeAgo is not None:
+            dateFromTimeAgo = self.__getPublicationDateFromTimeAgo(timeAgo)
+
+            datePublicationOfArticle = dateFromTimeAgo
+        
+        if datePublicationOfArticle is not None:
+            datePublicationOfArticle = self.__standarizeDate(datePublicationOfArticle)
+
+        return datePublicationOfArticle
+
+    
+    def __standarizeDate(self,date):
+
+        try:
+            date_dateTime = datetime.strptime(date,"%b/%d/%Y")
+
+            #dateStr = datetime.strftime(date_dateTime,"%b/%d/%Y")
+
+        except Exception as e:
+            print(f"Error standarizing {date} | {e}")
+            date_dateTime = None
+        return date_dateTime
+
+    def get_date_publish_str(self):
+        try:
+            dateStr = datetime.strftime(self.date_publish,"%b/%d/%Y")
+        except Exception as e:
+            print(f"Error converting date {datetime} to string | {e}")
+
+            dateStr = None
+        return dateStr
 
 
 class webScrapper:
@@ -71,21 +161,8 @@ class webScrapper:
                     return None
 
             except Exception as e:
+                print(f"Error - {e}")
                 return None
-        
-
-
-
-
-                
-
-
-        
-
-
-    
-
-
 
 
 
@@ -97,11 +174,15 @@ def minimal_task():
 
     try:
         for article in myWebScrapper.extractListOfArticles():
+
             article_obj = Article(article["header"],article["description"],article["imageURL"])
             articlesList.append(article_obj)
+        
 
     except Exception as e:
-        print (e)
+        print (f"************* Error {e}")
+    
+    print("Break")
 
 
 
