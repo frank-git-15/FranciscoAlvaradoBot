557768df8f1a49bf6759885eb1989511abf53d51
frank-git-15 <franciscoalvarrera15@gmail.com>
2024-06-19 18:55:40 -0600

    Open chrome browser and seach something in aljazeera.com

--- tasks.py
+++ tasks.py
@@ -1,13 +1,28 @@
 from robocorp.tasks import task
 
+from selenium import webdriver
+
+import time
+
+
+search_criteria = "Biden"
 
 class Article:
     def __init__(self,header,description,imgeUrl) -> None:
-        pass
+        self.header = header
+        self.description = description
+        self.imgeUrl = imgeUrl
 
 
 
 @task
 def minimal_task():
-    message = "Hello"
-    message = message + " World!"
+    with webdriver.Chrome() as driver:
+        articles_list = []
+        driver.implicitly_wait(10)
+        # driver.set_window_size(1024, 768)
+
+        driver.get(f"https://www.aljazeera.com/search/{search_criteria}?sort=date")
+
+        time.sleep(5)
+
